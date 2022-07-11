23c4a681ef81ca0ac146fe0ed4eb3a13f6b602f0
Yusuf <yusufozbilek970@gmail.com>
2022-07-11 19:52:58 +0300

    Basic Input Output Test added

---
+++ CodeTester.py
@@ -0,0 +1,12 @@
+class CodeTester:
+    def __init__(self,opt = []):
+        pass
+
+    def TestInOut(self,inp, out, options = []):
+        return True if inp == out else False
+
+    def MultipleInOut(self):
+        pass
+
+    def TestByRatio(self):
+        pass
